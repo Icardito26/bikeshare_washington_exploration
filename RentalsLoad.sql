055a30dc11f546322a7a5e15b24bb376fc0f2d92
Icardito26 <arthur124.tran@gmail.com>
2025-01-14 12:49:59 +0100

    feat: init bikeshare washington

---
+++ RentalsLoad.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA raw_rentals;
+
+
+SET VARIABLE path_input = '/Users/arthurtran/Library/CloudStorage/OneDrive-Personnel/Cours/Exploration & Visualisation Données/Projet/data/';
+SELECT getvariable('path_input');
+
+SELECT * FROM read_csv('/Users/arthurtran/Library/CloudStorage/OneDrive-Personnel/Cours/Exploration & Visualisation Données/Projet/data/rentals/202412-capitalbikeshare-tripdata.csv');
+
+CREATE OR REPLACE TABLE raw_rentals.rentals_v1 AS
+SELECT * 
+FROM read_csv(getvariable('path_input') || '/rentals/*.csv');
+
+
+
+CREATE OR REPLACE TABLE raw_gbfs.station_information AS
+SELECT *
+FROM read_json('https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/station_information.json');
